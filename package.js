544f796ae1f8d479c35c78e767b251f69f01bc69
Tim Heckel <tim@slatebox.com>
2015-01-27 13:32:51 -0600

    Refactoring template instance html into string-based injections

--- package.js
+++ package.js
@@ -6,7 +6,6 @@ Package.on_use(function (api) {
     api.use(['jquery', 'underscore', 'templating'], 'client');
     api.add_files([
          'lib/client/meteor.paginator.js'
-        , 'lib/client/paginationTemplates.html'
         , 'lib/client/css/pagination.css'
     ], 'client');
 });
